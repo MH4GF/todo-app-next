9e09398b420e54d6f57862540d931dbfce1eb193
MH4GF <rixi436@gmail.com>
2021-04-28 16:37:10 +0900

    fix type api response

--- src/hooks/usecases/useTodosList.ts
+++ src/hooks/usecases/useTodosList.ts
@@ -1,6 +1,5 @@
 import { Todo } from "~/types/Todo";
 import axios from "axios";
-import { ApiResponse } from "~/types/api/ApiResponse";
 import { useEffect, useState } from "react";
 
 type IUseTodosList = () => {
@@ -13,7 +12,7 @@ export const useTodosList: IUseTodosList = () => {
   const fetchTodosData = async () => {
     // TODO: エラーハンドリング
     // TODO: ローディング
-    const res = await axios.get<ApiResponse<Todo[]>>("/api/todos");
+    const res = await axios.get<{ result: Todo[] }>("/api/todos");
     setResult(res.data.result);
   };
 
